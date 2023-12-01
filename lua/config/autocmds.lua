2e781ef1db9e03e27f86478efec19ddebaf3879f
Tyler Hallada <tyler@hallada.net>
2023-12-01 15:28:11 -0500

    Turn off cursorline in terminal

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,9 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Turn off cursorline in terminal windows
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  command = "setlocal listchars= | set nocursorline | set nocursorcolumn",
+})
