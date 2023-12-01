568681ce4cf324f18b5729a619a8e110c8f1603f
Tyler Hallada <tyler@hallada.net>
2023-12-01 15:26:03 -0500

    Fix gruvbox sign backgrounds

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -7,5 +7,16 @@ return {
       dark1 = "#242424",
     },
     dim_inactive = true,
+    overrides = {
+      GruvboxAquaSign = { bg = "NONE" },
+      GruvboxBlueSign = { bg = "NONE" },
+      GruvboxGreenSign = { bg = "NONE" },
+      GruvboxOrangeSign = { bg = "NONE" },
+      GruvboxPurpleSign = { bg = "NONE" },
+      GruvboxRedSign = { bg = "NONE" },
+      GruvboxYellowSign = { bg = "NONE" },
+      SignColumn = { bg = "NONE" },
+      FoldColumn = { bg = "NONE" },
+    },
   },
 }
