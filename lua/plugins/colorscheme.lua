4d1f95a7d8d770fecbf55ea5667100bca1a27862
Tyler Hallada <tyler@hallada.net>
2023-12-01 15:28:12 -0500

    Remove sunglasses
    It's worse than just normal dim_inactive. It was okay with moonbow, but I think I'm not going to normally use moonbow.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,13 +6,6 @@ return {
     },
   },
   {
-    "miversen33/sunglasses.nvim",
-    opts = {
-      filter_type = "SHADE",
-      filter_percent = 0.2,
-    },
-  },
-  {
     "ellisonleao/gruvbox.nvim",
     opts = {
       contrast = "hard",
