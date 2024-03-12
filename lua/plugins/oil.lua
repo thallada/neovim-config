f816c00dc7cef15aeed9cf42af14c56df62167a4
Tyler Hallada <tyler@hallada.net>
2024-03-12 10:21:50 -0400

    Updates and change some annoying oil.nvim mappings

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,28 @@
 return {
   "stevearc/oil.nvim",
-  opts = {},
+  opts = {
+    -- remapping is not working, so redefine defaults with C-l and C-h swapped for C-f and C-i
+    -- see: https://github.com/stevearc/oil.nvim/issues/217
+    use_default_keymaps = false,
+    keymaps = {
+      ["g?"] = "actions.show_help",
+      ["<CR>"] = "actions.select",
+      ["<C-s>"] = "actions.select_vsplit",
+      ["<C-i>"] = "actions.select_split",
+      ["<C-t>"] = "actions.select_tab",
+      ["<C-p>"] = "actions.preview",
+      ["<C-c>"] = "actions.close",
+      ["<C-f>"] = "actions.refresh",
+      ["-"] = "actions.parent",
+      ["_"] = "actions.open_cwd",
+      ["`"] = "actions.cd",
+      ["~"] = "actions.tcd",
+      ["gs"] = "actions.change_sort",
+      ["gx"] = "actions.open_external",
+      ["g."] = "actions.toggle_hidden",
+      ["g\\"] = "actions.toggle_trash",
+    },
+  },
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
     { "-", "<cmd>Oil<cr>", { desc = "Open parent directory in current buffer" } },
