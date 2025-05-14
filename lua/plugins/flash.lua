a060053a3f954fd103dca0468cdad29c5b28d1d3
Tyler Hallada <tyler@hallada.net>
2025-05-14 17:32:11 -0400

    Disable flash for line-level char find
    It's not working for me and I cannot figure out why

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/flash.nvim",
+  opts = {
+    modes = {
+      char = {
+        enabled = false, -- idk why but this does not work for me, ';' and ',' do not repeat
+      },
+    },
+  },
+}
